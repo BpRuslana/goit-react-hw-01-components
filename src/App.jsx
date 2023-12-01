234efaa5caa222872c194892f6dc05685e087d61
BpRuslana <139804632+BpRuslana@users.noreply.github.com>
2023-12-01 15:51:05 +0100

    added index and app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Statistics } from 'components/Statistics/Statistics';
+import user from './assets/user.json'
+import data from './assets/data.json'
+import friends from './assets/friends.json';
+import {Profile} from './components/Profile/Profile';
+import { FriendsList } from './components/FriendsList/FriendsList';
+import { TransactionHistory } from './components/Transaction/TransactionHistory';
+import transactions from './assets/transactions.json'
+
+import 'modern-normalize';
+
+
+function App() {
+  return (
+    <div>
+      <Profile user={user}/>
+      <Statistics title="Upload stats" stats={data}/>
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
+
+export default App;
